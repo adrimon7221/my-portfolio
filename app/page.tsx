e291f6b115db19ad53625358600ff87b75cc1c17
adrimon7221 <adrimon7221@gmail.com>
2025-12-02 00:56:51 -0500

    fix: improving responsive design in the About section

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import WorkSection from './_components/sections/WorkSection';
 import ArticlesSection from './_components/sections/ArticlesSection';
 import ContactSection from './_components/sections/ContactSection';
 import Navbar from './_components/ui/Navbar';
+import AboutSection from './_components/sections/AboutSection';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <AboutSection />
       <ProjectsSection />
       <WorkSection />
       <ArticlesSection />
